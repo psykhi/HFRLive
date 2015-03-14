9034377e47e9d454ba5131582de41c7f91946a1b
Alex <alexandre.pesant@gmail.com>
2015-03-14 23:38:45 +0100

    Changed description + GA

--- popup.js
+++ popup.js
@@ -101,6 +101,17 @@ function onNotificationStatusChange(state)
 /**
  * Actual script : we wait for the DOM to be loaded
  */
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-60766637-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 document.addEventListener('DOMContentLoaded', function() {
     button = document.getElementById("refresh_button");
     button.addEventListener('click', onRefreshButtonClicked);
